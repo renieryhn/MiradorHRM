6f2e3e84a2e4a51e84980b75e3adafa33d635b27
Reniery Rodriguez <reniery@gmail.com>
2024-03-19 08:34:13 -0600

    Creacion de Cargo y Producto

---
+++ pruebaTemplate/Controllers/CargoController.cs
@@ -0,0 +1,256 @@
+﻿using System.Data;
+using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using static PlanillaPM.cGeneralFun;
+using Microsoft.AspNetCore.Identity;
+using PlanillaPM.ViewModel;
+
+using PlanillaPM.Models;
+
+namespace PlanillaPM.Controllers
+{
+    public class CargoController : Controller
+    {
+        private readonly PlanillaContext _context;
+        private readonly UserManager<Usuario> _userManager;
+
+        public CargoController(PlanillaContext context, UserManager<Usuario> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Cargo
+        public async Task<IActionResult> Index(int pg, string? filter)
+        {
+            List<Cargo> registros;
+            if (filter != null)
+            {
+                registros = await _context.Cargos.Where(r => r.NombreCargo.ToLower().Contains(filter.ToLower())).ToListAsync();
+            }
+            else
+            {
+                registros = await _context.Cargos.ToListAsync();
+            }
+            const int pageSize = 10;
+            if (pg < 1) pg = 1;
+            int recsCount = registros.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = registros.Skip(recSkip).Take(pager.PageSize).ToList();
+            this.ViewBag.Pager = pager;
+            return View(data);
+        }
+         public ActionResult Download()
+         {
+             ListtoDataTableConverter converter = new ListtoDataTableConverter();
+             List<Cargo>? data = null;
+             if (data == null)
+             {
+                data = _context.Cargos.ToList();
+             }
+             DataTable table = converter.ToDataTable(data);
+             string fileName = "Cargos.xlsx";
+             using (XLWorkbook wb = new XLWorkbook())
+             {
+                 wb.Worksheets.Add(table);
+                 using (MemoryStream stream = new MemoryStream())
+                 {
+                     wb.SaveAs(stream);
+                     return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+                 }
+             }
+        }    
+        // GET: Cargo/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cargo = await _context.Cargos
+                .FirstOrDefaultAsync(m => m.IdCargo == id);
+            if (cargo == null)
+            {
+                return NotFound();
+            }
+
+            return View(cargo);
+        }
+
+        // GET: Cargo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cargo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCargo,NombreCargo,Activo,FechaCreacion,FechaModificacion,CreadoPor,ModificadoPor")] Cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                SetCamposAuditoria(cargo, false);
+                _context.Add(cargo);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "El registro ha sido creado exitosamente.";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                var message = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+                TempData["error"] = "Error: " + message;
+            }
+            return View(cargo);
+        }
+
+        // GET: Cargo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cargo = await _context.Cargos.FindAsync(id);
+            if (cargo == null)
+            {
+                return NotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: Cargo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCargo,NombreCargo,Activo,FechaCreacion,FechaModificacion,CreadoPor,ModificadoPor")] Cargo cargo)
+        {
+            if (id != cargo.IdCargo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    SetCamposAuditoria(cargo, false);
+                    _context.Update(cargo);
+                    await _context.SaveChangesAsync();
+                    TempData["success"] = "El registro ha actualizado exitosamente.";
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CargoExists(cargo.IdCargo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }            
+            else
+            {
+                var message = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+                TempData["Error"] = "Error: " + message;
+            }
+            return View(cargo);
+        }
+
+        // GET: Cargo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cargo = await _context.Cargos
+                .FirstOrDefaultAsync(m => m.IdCargo == id);
+            if (cargo == null)
+            {
+                return NotFound();
+            }
+
+            return View(cargo);
+        }
+
+        // POST: Cargo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+             var cargo = await _context.Cargos.FindAsync(id);
+            try
+            {
+               
+                if (cargo != null)
+                {
+                    _context.Cargos.Remove(cargo);
+                    await _context.SaveChangesAsync();
+                    TempData["success"] = "El registro ha sido eliminado exitosamente.";
+                    return RedirectToAction(nameof(Index));
+                } 
+                else
+                {
+                    TempData["Error"] = "Hubo un error al intentar eliminar el Empleado Contacto. Por favor, verifica la información e intenta nuevamente.";
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException != null && ex.InnerException.Message.Contains("FK_"))
+                {
+                    TempData["error"] = "Error: No puede elimiar el registro actual ya que se encuentra relacionado a otro Registro.";
+                }
+                else
+                {
+                    var message = ex.InnerException;
+                    TempData["error"] = "Error: " + message;
+                }
+                return View(cargo);
+            }
+
+        }
+
+        private bool CargoExists(int id)
+        {
+            return _context.Cargos.Any(e => e.IdCargo == id);
+        }
+        
+        private void SetCamposAuditoria(Cargo record, bool bNewRecord)
+        {
+            var now = DateTime.Now;
+            var CurrentUser =  _userManager.GetUserName(User);
+           
+            if (bNewRecord)
+            {
+                record.FechaCreacion = now;
+                record.CreadoPor = CurrentUser;
+                record.FechaModificacion = now;
+                record.ModificadoPor = CurrentUser;
+                record.Activo = true;
+            }
+            else
+            {
+                record.FechaModificacion = now;
+                record.ModificadoPor = CurrentUser;
+            }
+        }        
+    }
+}
---
+++ pruebaTemplate/Controllers/ProductoController.cs
@@ -0,0 +1,256 @@
+﻿using System.Data;
+using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using static PlanillaPM.cGeneralFun;
+using Microsoft.AspNetCore.Identity;
+using PlanillaPM.ViewModel;
+
+using PlanillaPM.Models;
+
+namespace PlanillaPM.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly PlanillaContext _context;
+        private readonly UserManager<Usuario> _userManager;
+
+        public ProductoController(PlanillaContext context, UserManager<Usuario> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Producto
+        public async Task<IActionResult> Index(int pg, string? filter)
+        {
+            List<Producto> registros;
+            if (filter != null)
+            {
+                registros = await _context.Productos.Where(r => r.NombreProducto.ToLower().Contains(filter.ToLower())).ToListAsync();
+            }
+            else
+            {
+                registros = await _context.Productos.ToListAsync();
+            }
+            const int pageSize = 10;
+            if (pg < 1) pg = 1;
+            int recsCount = registros.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = registros.Skip(recSkip).Take(pager.PageSize).ToList();
+            this.ViewBag.Pager = pager;
+            return View(data);
+        }
+         public ActionResult Download()
+         {
+             ListtoDataTableConverter converter = new ListtoDataTableConverter();
+             List<Producto>? data = null;
+             if (data == null)
+             {
+                data = _context.Productos.ToList();
+             }
+             DataTable table = converter.ToDataTable(data);
+             string fileName = "Productos.xlsx";
+             using (XLWorkbook wb = new XLWorkbook())
+             {
+                 wb.Worksheets.Add(table);
+                 using (MemoryStream stream = new MemoryStream())
+                 {
+                     wb.SaveAs(stream);
+                     return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+                 }
+             }
+        }    
+        // GET: Producto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .FirstOrDefaultAsync(m => m.IdProducto == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // GET: Producto/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Producto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdProducto,NombreProducto,CodigoProducto,Descripcion,Activo,FechaCreacion,FechaModificacion,CreadoPor,ModificadoPor")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                SetCamposAuditoria(producto, false);
+                _context.Add(producto);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "El registro ha sido creado exitosamente.";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                var message = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+                TempData["error"] = "Error: " + message;
+            }
+            return View(producto);
+        }
+
+        // GET: Producto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Producto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdProducto,NombreProducto,CodigoProducto,Descripcion,Activo,FechaCreacion,FechaModificacion,CreadoPor,ModificadoPor")] Producto producto)
+        {
+            if (id != producto.IdProducto)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    SetCamposAuditoria(producto, false);
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                    TempData["success"] = "El registro ha actualizado exitosamente.";
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductoExists(producto.IdProducto))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }            
+            else
+            {
+                var message = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+                TempData["Error"] = "Error: " + message;
+            }
+            return View(producto);
+        }
+
+        // GET: Producto/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .FirstOrDefaultAsync(m => m.IdProducto == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // POST: Producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+             var producto = await _context.Productos.FindAsync(id);
+            try
+            {
+               
+                if (producto != null)
+                {
+                    _context.Productos.Remove(producto);
+                    await _context.SaveChangesAsync();
+                    TempData["success"] = "El registro ha sido eliminado exitosamente.";
+                    return RedirectToAction(nameof(Index));
+                } 
+                else
+                {
+                    TempData["Error"] = "Hubo un error al intentar eliminar el Empleado Contacto. Por favor, verifica la información e intenta nuevamente.";
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException != null && ex.InnerException.Message.Contains("FK_"))
+                {
+                    TempData["error"] = "Error: No puede elimiar el registro actual ya que se encuentra relacionado a otro Registro.";
+                }
+                else
+                {
+                    var message = ex.InnerException;
+                    TempData["error"] = "Error: " + message;
+                }
+                return View(producto);
+            }
+
+        }
+
+        private bool ProductoExists(int id)
+        {
+            return _context.Productos.Any(e => e.IdProducto == id);
+        }
+        
+        private void SetCamposAuditoria(Producto record, bool bNewRecord)
+        {
+            var now = DateTime.Now;
+            var CurrentUser =  _userManager.GetUserName(User);
+           
+            if (bNewRecord)
+            {
+                record.FechaCreacion = now;
+                record.CreadoPor = CurrentUser;
+                record.FechaModificacion = now;
+                record.ModificadoPor = CurrentUser;
+                record.Activo = true;
+            }
+            else
+            {
+                record.FechaModificacion = now;
+                record.ModificadoPor = CurrentUser;
+            }
+        }        
+    }
+}
